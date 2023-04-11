4ee29fd3a36ac6d594ed67b12cd6978c3ae2d4b6
Katy Morris <katydmorris@gmail.com>
2023-04-10 18:55:54 -0600

    push

---
+++ my-project/pages/index.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <div className="flex relative min-h-screen flex-col items-center justify-between p-0" style={{ minHeight: '100vh' }}>
+      <div style={{
+        backgroundImage: `url('/background2.svg')`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        width: '100%',
+        height: '100%',
+        minHeight: '100%',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+      }}>
+        <div style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          textAlign: 'center',
+          zIndex: 1,
+        }}>
+          <Image src="/logo.png" width={1000} height={1000} alt="Logo" />
+          <p className='mt-24 text-white text-xl'>kathy@emirolabs.com</p>
+        </div>
+      </div>
+    </div>
+  );
+}
